c32d978342639725e7e38200b8f8556e18a6cb0c
Doug <karaballo.douglas@hotmail.com>
2021-08-16 10:25:00 -0400

    Correcion de error en el controlador de empleados en la funcion de eliminar

--- controladores/empleadosC.php
+++ controladores/empleadosC.php
@@ -112,7 +112,7 @@ class EmpleadosC{
             $respuesta = EmpleadosM::BorrarEmpleadoM($datosC, $tablaBD);
 
             if($respuesta == "Bien" ){
-                header("locationiIndex.php?ruta=empleados");
+                header("location:index.php?ruta=empleados");
             }else{
                 echo "Error!!!";
             }
